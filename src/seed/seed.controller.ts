9a445873b3224a6e223836eb5ace54f5ab6db899
Daniel <leinadpy@gmail.com>
2022-09-13 00:09:02 -0400

    Nest prod v1.0.2-rc1

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -12,7 +12,7 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  @Auth(ValidRoles.admin)
+  // @Auth(ValidRoles.admin)
   executeSeed() {
     return this.seedService.runSeed();
   }
